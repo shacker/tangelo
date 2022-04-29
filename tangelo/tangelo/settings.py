947e607af36a0d62627ca3a1330c32786286abeb
Scot Hacker <shacker@birdhouse.org>
2022-04-28 23:25:45 -0700

    Add django_extensions, gallery app

--- tangelo/tangelo/settings.py
+++ tangelo/tangelo/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    "django_extensions",
 ]
 
 MIDDLEWARE = [
@@ -49,7 +50,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'tangelo.urls'
+ROOT_URLCONF = 'tangelo.tangelo.urls'
 
 TEMPLATES = [
     {
@@ -67,7 +68,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'tangelo.wsgi.application'
+WSGI_APPLICATION = 'tangelo.tangelo.wsgi.application'
 
 
 # Database
