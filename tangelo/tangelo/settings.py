03dc2e10889c3cb0cc1ebf65a57cfa0313b35040
Scot Hacker <shacker@birdhouse.org>
2022-05-12 22:45:22 -0700

    Basic bootstrap design

--- tangelo/tangelo/settings.py
+++ tangelo/tangelo/settings.py
@@ -117,8 +117,9 @@ USE_TZ = True
 # Static and media files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = "static/"
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATIC_URL = "/static/"
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 MEDIA_URL = "/media/"
