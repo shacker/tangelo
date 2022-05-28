1d7fe138eeba85f2571a0abcd02d6c0533f523ad
Scot Hacker <shacker@birdhouse.org>
2022-05-27 22:59:07 -0700

    Fix album thumbs on homepage

---
+++ gallery/templatetags/album_thumb.py
@@ -0,0 +1,21 @@
+from django import template
+from django.conf import settings
+from django.shortcuts import reverse
+
+register = template.Library()
+
+
+@register.inclusion_tag("thumbnails/album_thumb.html")
+def album_thumb(album, *args, **kwargs):
+    """Generate data needed for generating an ALBUM thumbnail into
+    inclusion template in decorator.
+    """
+    album_url = reverse("album", kwargs={"slug": album.slug})
+
+    embed_url = (
+        album.cat_thumb.get_embed_url(size=settings.FLICKR_THUMBNAIL_SIZE)
+        if album.cat_thumb
+        else None
+    )
+
+    return {"local_url": album_url, "embed_url": embed_url, "title": album.title}
--- gallery/templatetags/image_thumb.py
+++ gallery/templatetags/image_thumb.py
@@ -1,18 +1,19 @@
+from threading import local
 from django import template
 from django.conf import settings
 from django.shortcuts import reverse
 
 register = template.Library()
 
+from icecream import ic
 
-@register.inclusion_tag("_thumbnail.html")
-def render_thumbnail(img, *args, **kwargs):
-    """Generate data needed for generating a thumbnail and render it into
-    inclusion template  `_thumbnail.html`
+@register.inclusion_tag("thumbnails/image_thumb.html")
+def image_thumb(img, *args, **kwargs):
+    """Generate data needed for generating an IMAGE thumbnail into
+    inclusion template in decorator.
     """
     local_url = reverse("image", kwargs={"album_slug": img.album.slug, "flickr_id": img.flickr_id})
     embed_url = img.get_embed_url(size=settings.FLICKR_THUMBNAIL_SIZE)
-
     return {
         "local_url": local_url,
         "embed_url": embed_url,
